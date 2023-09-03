886925e07f25aa50d576c065ee705dc9591ea69c
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2023-09-03 21:08:08 +0530

    LocationHistory page editted

--- safepass_mobile/screen/LocationHistoryScreen/LocationHistoryScreen.js
+++ safepass_mobile/screen/LocationHistoryScreen/LocationHistoryScreen.js
@@ -1,20 +1,133 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { colors } from "../../theme";
+import Background from "../background";
+import ScreenWrapper from "../../components/screenWrapper";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+
+const history = [
+  {
+    id: 1,
+    animalName: "Deer",
+    distance: "5km",
+    time: "10 min ago",
+  },
+  {
+    id: 2,
+    animalName: "Elephant",
+    distance: "5km",
+    time: "10 min ago",
+  },
+  {
+    id: 3,
+    animalName: "Mongoose",
+    distance: "5km",
+    time: "10 min ago",
+  },
+  {
+    id: 4,
+    animalName: "Dog",
+    distance: "5km",
+    time: "10 min ago",
+  },
+  {
+    id: 5,
+    animalName: "Dog",
+    distance: "5km",
+    time: "10 min ago",
+  },
+  {
+    id: 6,
+    animalName: "Dog",
+    distance: "5km",
+    time: "10 min ago",
+  },
+  {
+    id: 7,
+    animalName: "Dog",
+    distance: "5km",
+    time: "10 min ago",
+  },
+  {
+    id: 8,
+    animalName: "Dog",
+    distance: "5km",
+    time: "10 min ago",
+  },
+];
 
 const LocationHistoryScreen = (props) => {
-  const { container } = styles;
+  const { container, title, card, animalName, distance, time } = styles;
   return (
-    <View style={container}>
-      <Text>LocationHistoryScreen</Text>
-    </View>
+    <Background>
+      <ScreenWrapper style={container}>
+        <Text style={title}>Location History</Text>
+        <ScrollView style={styles.scrollView}>
+          <FlatList
+            data={history}
+            keyExtractor={(item) => item.id.toString()}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <TouchableOpacity style={card} onPress={() => {}}>
+                <View>
+                  <Text style={animalName}>{item.animalName}</Text>
+                  <Text style={distance}>{item.distance}</Text>
+                  <Text style={time}>{item.time}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
+        </ScrollView>
+      </ScreenWrapper>
+    </Background>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+    color: colors.primary,
+  },
+  card: {
+    backgroundColor: "white",
+    width: 160,
+    padding: 16,
+    borderRadius: 12,
+    marginBottom: 10,
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  animalName: {
+    ...colors.heading,
+    fontWeight: "bold",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  distance: {
+    ...colors.heading,
+    fontSize: 12,
+    textAlign: "center",
+  },
+  time: {
+    ...colors.heading,
+    fontSize: 8,
+    textAlign: "center",
   },
 });
 
